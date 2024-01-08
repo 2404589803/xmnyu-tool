c7b2754a3924029020c91d5503e2d683fb339760
King guys <68528929+2404589803@users.noreply.github.com>
2024-01-09 01:07:47 +0800

    update file

--- main.py
+++ main.py
@@ -9,21 +9,31 @@ class Spider:
         self.__real_base_url = ''  # 用于存储实际的基础 URL 的私有属性
         self.__base_url = url  # 存储用户提供的基础 URL 的私有属性
         self.__headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Content-Length': '161',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Host': '59.61.86.138:18080',
+            'Origin': 'http://59.61.86.138:18080',
+            'Pragma': 'no-cache',
+            'Referer': 'http://59.61.86.138:18080/',
+            'Upgrade-Insecure-Requests': '1',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
         }  # 存储请求头的私有属性，模拟浏览器请求
         self.session = requests.Session()  # 创建一个会话对象，用于跨多个请求保持会话状态
 
-    def __set_real_url(self):
-        # 私有方法，用于获取实际的基础 URL
-        request = self.session.get(self.__base_url, headers=self.__headers)  # 发送 GET 请求获取网页内容
-        real_url = request.url  # 获取请求的实际 URL
-        self.__real_base_url = real_url[:len(real_url) - len('default2.aspx')]  # 剔除默认的后缀部分，得到实际的基础 URL
-        return request  # 返回请求对象
-    
-    def get_cookie():
+    def save_cookie(self, response):
+        # 保存从响应中获取的Cookie到会话中
+        cookies = response.cookies
+        self.session.cookies.update(cookies)
 
-        request = requests.get('http://59.61.86.138:18080') 
+    def send_request(self):
+        # 发送 GET 请求
+        response = self.session.get(self.__base_url, headers=self.__headers)
+        self.save_cookie(response)  # 保存Cookie到会话中
+        return response  # 返回响应对象
 
-        cookie = requests.cookie
 
-        return cookie
